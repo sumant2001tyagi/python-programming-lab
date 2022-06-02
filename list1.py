dcc8b0fb7d5032151e326b4c4f5e030383063eac
sumant2001tyagi <96302593+sumant2001tyagi@users.noreply.github.com>
2022-06-02 22:55:20 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,6 @@
+# python program to print the list of student but in which one student take the teesee and go to another school
+student = ["ram" ,"shyam" ,"harsh" ,"krishna"]
+for students in student:
+    if students == "harsh":
+        continue;
+    print(students)
