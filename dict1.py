d9327ba8cb7e03ac4d05de478f221a452040b68a
sumant2001tyagi <96302593+sumant2001tyagi@users.noreply.github.com>
2022-06-02 15:28:32 +0530

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,13 @@
+
+my_dict = {}
+
+
+my_dict = {1: 'apple', 2: 'ball'}
+
+
+my_dict = {'name': 'John', 1: [2, 4, 3]}
+
+
+my_dict = dict({1:'apple', 2:'ball'})
+
+my_dict = dict([(1,'apple'), (2,'ball')])
